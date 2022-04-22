e1065d89798ddb952c40fc97f5d282766c2cb6fc
zhangziyue.gloriae <zhangziyue.gloriae@bytedance.com>
2022-04-22 16:07:18 +0800

    feat:按照点赞量排序

--- app/src/main/java/com/gloria/gloriavideo/MainActivity.kt
+++ app/src/main/java/com/gloria/gloriavideo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.LinearLayout
 import com.gloria.common.card.CardLayout
 import com.gloria.common.network.VideoApi
@@ -12,11 +13,15 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
     private var linearLayout: LinearLayout? = null
+    private var btnSortDefault: Button? = null
+    private var btnSortLike: Button? = null
+
     private var scope: CoroutineScope? = null
     private var videoList: List<VideoBean>? = null
 
@@ -25,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide();// 隐藏ActionBar
         setContentView(R.layout.activity_main)
 
+        scope = MainScope()
         initView()
         initAction()
     }
@@ -36,19 +42,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView() {
         linearLayout = findViewById(R.id.ll_show_cards)
+        btnSortDefault = findViewById(R.id.btn_sort_default)
+        btnSortLike = findViewById(R.id.btn_sort_like)
     }
 
     private fun initAction() {
         getVideoData()
+        btnSortDefault?.setOnClickListener {
+            scope?.launch { setVideoData(false) }
+        }
+        btnSortLike?.setOnClickListener {
+            scope?.launch { setVideoData(true) }
+        }
     }
 
     private fun getVideoData() {
-        scope = MainScope()
         scope?.launch {
             try {
                 videoList = VideoApi.createVideoApi().requestData()
                 Log.d(TAG, videoList.toString())
-                setVideoData()
+                setVideoData(false)
             }catch (e: Exception){
                 e.printStackTrace()
                 Log.e(TAG, e.toString())
@@ -56,8 +69,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setVideoData() {
-        for(item in videoList!!) {
+    private fun setVideoData(sortByLike: Boolean) {
+        var showVideoList = ArrayList<VideoBean>()
+        if (sortByLike){
+            for (item in videoList!!){
+                showVideoList.add(item)
+            }
+            Collections.sort(showVideoList, object : Comparator<VideoBean> {
+                override fun compare(p0: VideoBean?, p1: VideoBean?): Int {
+                    if (p0?.getLikeInt()!! > p1?.getLikeInt()!!){
+                        return -1
+                    }
+                    if (p0?.getLikeInt()!! < p1?.getLikeInt()!!){
+                        return 1
+                    }
+                    return 0
+                }
+            })
+        }else{
+            showVideoList = videoList as ArrayList<VideoBean>
+        }
+        linearLayout?.removeAllViews()
+        for(item in showVideoList!!) {
             val card = CardLayout(this, null)
             linearLayout?.addView(card)
             card.setVideoBean(item)
